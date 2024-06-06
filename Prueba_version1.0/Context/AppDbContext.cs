76a235b9287ccf90b0639308c331d745f3fc22e6
esteban.pizarro <esteban.pizarro@TDIGITAL-EPIZA>
2024-06-06 16:09:36 -0400

    Agregar archivos de proyecto.

---
+++ Prueba_version1.0/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prueba_version1._0.Models;
+
+namespace Prueba_version1._0.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<GestionPersona> GestionPersonas  { get; set; } //colecciones
+
+    }
+}
---
+++ Prueba_version1.0/Models/GestionPersona.cs
@@ -0,0 +1,13 @@
+﻿namespace Prueba_version1._0.Models
+{
+    public class GestionPersona
+    {
+        public int Id { get; set; }
+        public string NombrePersona { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string? CorreoPersona { get; set; }
+        public string? DireccionPersona { get; set; }
+        public string? TelefonoPersona { get; set; }
+    }
+}
